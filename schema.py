3023cd0feb46d406c31a1323f0f7e7749532f9db
ckmonish2000 <ckmonish2000@gmail.com>
2020-08-08 21:11:16 +0530

    queries

--- schema.py
+++ schema.py
@@ -1,8 +1,23 @@
 import graphene
 import json
+from datetime import datetime
+
+class user(graphene.ObjectType):
+    id=graphene.ID()
+    name=graphene.String()
+    last_login=graphene.DateTime()
 
 class Query(graphene.ObjectType):
     isstaff=graphene.Boolean(name="is_staff")
+    users=graphene.List(user,first=graphene.Int())
+
+    def resolve_users(self,info,first):
+        return [
+            user(name="mk" ,last_login=datetime.now()),
+            user(name="jk" ,last_login=datetime.now()),
+            user(name="ck" ,last_login=datetime.now()),
+            user(name="sk" ,last_login=datetime.now()),
+        ][first:]
 
     def resolve_isstaff(self,info):
         return True
@@ -11,7 +26,10 @@ schema=graphene.Schema(query=Query)
 
 result=schema.execute('''
 {
-    is_staff
+    is_staff,
+    people:users(first:2){
+        name
+    }
 }
 '''
 )
