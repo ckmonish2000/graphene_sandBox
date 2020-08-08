21b6073216c98506e9109ae742b088012232a544
ckmonish2000 <ckmonish2000@gmail.com>
2020-08-08 21:45:39 +0530

    working custom field

--- testSchema.py
+++ testSchema.py
@@ -12,15 +12,12 @@ class Friends(graphene.ObjectType):
 class Query(graphene.ObjectType):
     name=graphene.String(firstName=graphene.String(default_value="Dr."),lastName=graphene.String(default_value="strange"))
     age=graphene.Int(num=graphene.Int(default_value=20))
-    friend=Friends
+    friend=graphene.NonNull(Friends,first=graphene.String(),last=graphene.String())
 
-    def resolve_friend(root,info):
-        return[
-            Friends(first_name="rahul",last_name="jain"),
-            Friends(first_name="md.",last_name="saquib"),
-            Friends(first_name="B",last_name="P"),
+    def resolve_friend(root,info,first,last):
+        return Friends(first_name=first,last_name=last)
+        
             
-        ]
 
     def resolve_name(root,info,firstName,lastName):
         f=firstName
@@ -38,12 +35,17 @@ result=schema.execute('''
 {
     name(firstName:"m" , lastName:"k"),
     age(num:18),
+    friend(first:"joey",last:"tribbiani"){
+        firstName
+        lastName
+    }
+        
     
 }
 '''
 )
 
-print(result.data)
+print(json.dumps(dict(result.data),indent=4))
 
 
 
